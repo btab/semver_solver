5de6b1a804fac4a62ff5869c64207b673dc5efdb
andre <andre@bluetheta.com>
2016-02-04 22:07:07 -0800

    Added some basic data structures and a solver stub.

--- main.go
+++ main.go
@@ -1,16 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"log"
 
 	"github.com/blang/semver"
 )
 
+var (
+	NoConstraints = []Constraint{}
+)
+
 func main() {
-	v, _ := semver.Make("0.0.1-alpha.preview+123.github")
-	fmt.Printf("Major: %d\n", v.Major)
-	fmt.Printf("Minor: %d\n", v.Minor)
-	fmt.Printf("Patch: %d\n", v.Patch)
-	fmt.Printf("Pre: %s\n", v.Pre)
-	fmt.Printf("Build: %s\n", v.Build)
+	availableArtifacts := []Artifact{
+		{"A", semver.MustParse("1.0.0"), NoConstraints},
+		{"A", semver.MustParse("1.1.0"), NoConstraints},
+		{"A", semver.MustParse("1.2.0"), NoConstraints},
+		{"A", semver.MustParse("1.4.0"), NoConstraints},
+		{"A", semver.MustParse("2.1.0"), NoConstraints},
+		{"A", semver.MustParse("2.1.2"), NoConstraints},
+		{"B", semver.MustParse("1.1.0"), NoConstraints},
+		{"B", semver.MustParse("1.2.0"), NoConstraints},
+	}
+
+	constraints := []Constraint{
+		{"A", MustParseRange(">1.1.0")},
+	}
+
+	log.Println(availableArtifacts)
+	log.Println(constraints)
+
+	solution, err := Solve(constraints)
+
+	log.Println(solution)
+	log.Println(err)
+}
+
+type Artifact struct {
+	name      string
+	version   semver.Version
+	dependsOn []Constraint
+}
+
+type Constraint struct {
+	name string
+	rng  semver.Range
+}
+
+func Solve(constraints []Constraint) ([]Artifact, error) {
+	return nil, errors.New("no solution")
+}
+
+func MustParseRange(s string) semver.Range {
+	r, err := semver.ParseRange(s)
+
+	if err != nil {
+		panic(`semver: ParseRange(` + s + `): ` + err.Error())
+	}
+
+	return r
 }
