dfcaab25e5fe149e930813520bb77a75f5ee0a18
andre <andre@bluetheta.com>
2016-02-07 18:06:30 -0800

    Added convenience interface for building ConstraintSets.

--- constraint_set.go
+++ constraint_set.go
@@ -1,3 +1,29 @@
 package semver_solver
 
+import (
+	"github.com/blang/semver"
+)
+
 type ConstraintSet map[string][]Constraint
+
+func (cs ConstraintSet) AddConstraint(artifactName, cText string) error {
+	return cs.AddConstraintWithOrigin(artifactName, cText, nil)
+}
+
+func (cs ConstraintSet) AddConstraintWithOrigin(artifactName, cText string, origin *Artifact) error {
+	r, err := semver.ParseRange(cText)
+
+	if err != nil {
+		return err
+	}
+
+	c := Constraint{
+		RangeString: cText,
+		Range:       r,
+		Origin:      origin,
+	}
+
+	cs[artifactName] = append(cs[artifactName], c)
+
+	return nil
+}
